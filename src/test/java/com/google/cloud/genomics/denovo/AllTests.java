f9b4877566a0f201fcacb7d26d751dcae094521a
Subhodeep Moitra <smoitra@google.com>
2014-07-24 11:10:51 -0700

    Adds Test cases for Node class

--- src/test/java/com/google/cloud/genomics/denovo/AllTests.java
+++ src/test/java/com/google/cloud/genomics/denovo/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ BayesInferTest.class })
+@SuiteClasses({NodeTest.class, BayesInferTest.class})
 public class AllTests {
 
 }
