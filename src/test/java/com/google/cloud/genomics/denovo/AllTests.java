89e2d03176da25bc33c17ad264772ffcb2658ed6
Subhodeep Moitra <smoitra@google.com>
2014-07-24 12:29:05 -0700

    Adds DenovoBayesTest creation tests

--- src/test/java/com/google/cloud/genomics/denovo/AllTests.java
+++ src/test/java/com/google/cloud/genomics/denovo/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({NodeTest.class, BayesInferTest.class})
+@SuiteClasses({NodeTest.class, DenovoBayesNetTest.class, BayesInferTest.class})
 public class AllTests {
 
 }
