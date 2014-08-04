ddf8666496de58aef63a156b8b1f2b068fba1b31
Subhodeep Moitra <smoitra@google.com>
2014-08-04 15:08:37 -0700

    CommandLine access changed to private

--- src/main/java/com/google/cloud/genomics/denovo/GenomicsExperiment.java
+++ src/main/java/com/google/cloud/genomics/denovo/GenomicsExperiment.java
@@ -24,7 +24,7 @@ import java.io.IOException;
  */
 public class GenomicsExperiment {
 
-  static CommandLine cmdLine;
+  private static CommandLine cmdLine;
 
   public static void main(String[] args) throws IOException {
     System.out.println("-------- Starting Genomics Experiment ---------");
